a3cb66293003269adaefa94c2466f662bbe00db8
Payal Parida <payalparida@Payals-MacBook-Air.local>
2025-06-22 21:09:00 +0530

    Modified DataIngestion code to create chunks.

--- DataIngestion.py
+++ DataIngestion.py
@@ -1,13 +1,45 @@
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# def PDF_Loader(path):
+#     loader=PyPDFLoader(path)
+#     docs=loader.load()
+#     text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+#     text=text_splitter.split_documents(docs)
+#     return text
+
+# result=PDF_Loader('Data/CS5811_224428.pdf')
+# print(result[1])
+
+''' CODE 1: REPAIRED PDF FILE'''
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# def PDF_Loader(path):
+#     loader=PyPDFLoader(path)
+#     docs=loader.load()
+#     text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+#     text=text_splitter.split_documents(docs)
+#     return text
+
+# result=PDF_Loader('Data/CS5811_224428_Repaired.pdf')
+# print(result[5])
+
+''' CODE 2: USING DIFFERENT LOADER - PyMUPyMuPDFLoader '''
+
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def PDF_Loader(path):
-    loader=PyPDFLoader(path)
+    loader=PyMuPDFLoader(path)
     docs=loader.load()
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 
     text=text_splitter.split_documents(docs)
     return text
 
-result=PDF_Loader('Data/CS5811_224428.pdf')
-print(result[1])
+# result=PDF_Loader('Data/CS5811_224428.pdf')
+# print(result[5])
+
